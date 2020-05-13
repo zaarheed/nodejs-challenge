31c584a5dc65b66dad644903bdb62f68db4d72d6
zaarheed <zahid@zmdev.com>
2020-05-13 22:13:21 +0100

    include mongoose config

--- level-1/server.js
+++ level-1/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const api = require('./api');
+const mongooose = require("./config/mongoose");
 
 app.use(api);
 
-app.listen(3000, () => console.log('Listening on port 3000!'))
+app.listen(3000, () => {
+	console.log('Listening on port 3000!');
+	mongooose();
+})
